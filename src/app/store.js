38a45fde88da3f30f80dbe654dd98dfed3f44d2f
vikas-parmar <vkparmar003@gmail.com>
2023-05-10 06:19:49 +0530

    title update

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import todosReducer from "../features/todo/todoSlice";
+import { todosApi } from "../features/todo/todoApi";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [todosApi.reducerPath]: todosApi.reducer,
     todos: todosReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(todosApi.middleware),
 });
+
+setupListeners(store.dispatch);
